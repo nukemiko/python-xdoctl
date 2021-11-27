08c76e0fe6bbc8aa7837ad506335c67a443cb52a
nukemiko <north666dakota@gmail.com>
2021-11-27 12:36:19 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+def pkgver():
+    with open('VERSION', encoding='utf-8') as f:
+        _pkgver = f.read().strip()
+    return _pkgver
+
+
+setup(
+    name='python-xdoctl',
+    version=pkgver(),
+    packages=['xdoctl'],
+    url='https://github/nukemiko/python-xdoctl',
+    license='',
+    author='nukemiko',
+    author_email='north666dakota@gmail.com',
+    description='Xdotool 命令包装器',
+    python_requires='>=3.8',
+    platforms='linux',
+    keywords=['xdotool', 'libxdo', 'xdo', 'mouse', 'keyboard']
+)
